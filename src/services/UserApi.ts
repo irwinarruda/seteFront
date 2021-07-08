681b0659f0e0662fb5a6379585e1485f22214325
irwinarruda <arruda.irwin@gmail.com>
2021-07-08 10:41:32 -0300

    integration with .env variables

--- src/services/UserApi.ts
+++ src/services/UserApi.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-export const BASE_URL = 'https://seteapi.umarleyricardo.eti.br';
+export const BASE_URL = `${process.env.REACT_APP_API_URL}`;
 
 export const api = axios.create({
     baseURL: BASE_URL,
